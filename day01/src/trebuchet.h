d68bae42d83bcf23d6cae64eddeaa1deb2aa4b76
ToF_ <cthibauttof@gmail.com>
2023-12-03 11:30:40 +0100

    refactoring

--- day01/src/trebuchet.h
+++ day01/src/trebuchet.h
@@ -1,3 +1,4 @@
+#define TREBUCHET
 #define MAXLINES 2000
 #define LINEMAX 100
 char *Lines[MAXLINES];
@@ -7,4 +8,3 @@ void read_puzzle(char *);
 void free_puzzle();
 int digit_calibration_value(char *, int);
 int sum_calibration_values(int);
-
