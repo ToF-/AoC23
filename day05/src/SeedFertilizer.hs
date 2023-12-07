ef0f6a24d7c4ed814f66002aaa770ffb3362b5dc
Christophe Thibaut <cthibauttof@gmail.com>
2023-12-08 00:38:05 +0100

    day 05 toujours pas

--- day05/src/SeedFertilizer.hs
+++ day05/src/SeedFertilizer.hs
@@ -10,26 +10,26 @@ data Almanach = Almanach { seeds      :: [Int],
                            maps       :: [Map] }
     deriving (Eq, Show)
 
-data Map = Map [Converter]
+data Map = M [Converter]
     deriving (Eq, Show)
 
-data Converter = Converter { dest  :: Int,
+data Converter = C { dest  :: Int,
                              range :: Range }
     deriving (Eq, Show)
 
 
 readAlmanach :: [String] -> Almanach
 readAlmanach ss = Almanach { seeds      = map read (tail (words (head ss))),
-                             maps       = readMaps (Map []) (tail(tail ss)) }
+                             maps       = readMaps (M []) (tail(tail ss)) }
     where
         readMaps :: Map -> [String] -> [Map]
         readMaps m []        = [m]
-        readMaps m (s:ss) | null s  = m : readMaps (Map []) ss
+        readMaps m (s:ss) | null s  = m : readMaps (M []) ss
         readMaps m (s:ss) | (words s)!!1 == "map:" = readMaps m ss
-        readMaps (Map converters) (s:ss) =
-            readMaps (Map converters') ss
+        readMaps (M converters) (s:ss) =
+            readMaps (M converters') ss
                 where
-                    converters' = converters <> [Converter { dest = dst, range = (srce, len) }]
+                    converters' = converters <> [C { dest = dst, range = (srce, len) }]
                     dst  = ints!!0
                     srce = ints!!1
                     len  = ints!!2
@@ -42,21 +42,60 @@ seedRanges al = seedRanges' (seeds al)
         seedRanges' (s:l:ss) = (s,l) : seedRanges' ss
 
 convert :: Int -> Converter -> Maybe Int
-convert n (Converter dst (start,len)) =
+convert n (C dst (start,len)) =
     let offset = n - start
         within = offset >= 0 && offset < len
         result = dst + offset
     in if within then Just result else Nothing
 
 mapping :: Int -> Map -> Int
-mapping n (Map []) = n
-mapping n (Map (c:cs)) =
+mapping n (M []) = n
+mapping n (M (c:cs)) =
     let result = convert n c
      in case result of
           (Just m) -> m
-          Nothing -> mapping n (Map cs)
+          Nothing -> mapping n (M cs)
 
 lowestLocation :: Almanach -> Int
 lowestLocation sf = minimum $ foldl allMappings (seeds sf) [0..6]
     where
         allMappings seedList i = map (\seed -> mapping seed ((maps sf)!!i)) seedList
+
+isValidRange :: (Int, Int) -> Bool
+isValidRange (a,z) = a < z
+
+validRange :: (Int,Int) -> Maybe Range
+validRange (a,z) = if isValidRange (a,z)
+                      then Just (a, z - a + 1)
+                      else Nothing
+
+offset :: Int -> (Int,Int) -> Range
+offset off (a,z) = if isValidRange (a,z) then (a + off, z + off) else (0,0)
+
+convertR :: Range -> Converter -> [Range]
+convertR (start,len) (C dst (srce, clen)) =
+    let
+        (a0,z0) = (start, start + len - 1)
+        (a1,z1) = (srce, srce + clen - 1)
+        (a2,z2) = (max a0 a1, min z0 z1)
+        (a3,z3) = if isValidRange (a2,z2) then (a0,a1-1) else (0,0)
+        (a4,z4) = if isValidRange (a2,z2) then (z1+1,z0) else (0,0)
+    in case catMaybes $ map validRange [(a3,z3), offset (dst-srce) (a2,z2), (a4,z4)] of
+         [] -> [(start,len)]
+         rs -> rs
+
+mappingR :: [Range] -> Map -> [Range]
+mappingR rs (M []) = rs
+mappingR rs (M (c:cs)) = foldr convertAllRanges rs (c:cs)
+    where
+    convertAllRanges c rs = concatMap (\r -> convertR r c) rs
+
+allMappingsR :: [Range] -> [Map] -> [Range]
+allMappingsR rs ms = foldl (\acc m -> mappingR acc m) rs ms
+
+allMappingsRScan :: [Range] -> [Map] -> [[Range]]
+allMappingsRScan rs ms = scanl (\acc m -> mappingR acc m) rs ms
+
+lowestLocationR :: Almanach -> Int
+lowestLocationR sf = minimum $ map fst $ allMappingsR (seedRanges sf) (maps sf)
+
--- day05/test/SeedFertilizerSpec.hs
+++ day05/test/SeedFertilizerSpec.hs
@@ -48,18 +48,35 @@ spec = do
         it "can read an almanach" $ do
             seeds sf `shouldBe` [79,14,55,13]
             seedRanges sf `shouldBe` [(79,14),(55,13)]
-            (maps sf) !! 0 `shouldBe` Map [Converter {dest = 50, range = (98,2) }
-                                          ,Converter {dest = 52, range = (50,48) }]
-            (maps sf) !! 6 `shouldBe` Map [Converter {dest = 60, range = (56,37)}
-                                          ,Converter {dest = 56, range = (93,4)}]
+            (maps sf) !! 0 `shouldBe` M [C {dest = 50, range = (98,2) }
+                                          ,C {dest = 52, range = (50,48) }]
+            (maps sf) !! 6 `shouldBe` M [C {dest = 60, range = (56,37)}
+                                          ,C {dest = 56, range = (93,4)}]
 
     describe "converter" $ do
         it "converts an int within a range to a dest" $ do
-            let c = Converter { dest = 50, range = (98,2) }
+            let c = C { dest = 50, range = (98,2) }
             convert 98 c `shouldBe` Just 50
             convert 99 c `shouldBe` Just 51
             convert 100 c `shouldBe` Nothing
             convert 97 c `shouldBe` Nothing
+
+        it "converts a range of ints within a range to dest ranges" $ do
+            let c = C { dest = 50, range = (98,2) }
+            convertR (98,2) c `shouldBe` [(50,2)]
+            convertR (98,4) c `shouldBe` [(50,2),(100,2)]
+            convertR (96,4) c `shouldBe` [(96,2),(50,2)]
+            convertR (96,6) c `shouldBe` [(96,2),(50,2),(100,2)]
+            convertR (1,109) c `shouldBe` [(1,97),(50,2),(100,10)]
+            convertR (1,10) c `shouldBe` [(1,10)]
+            convertR (1,99) c `shouldBe` [(1,97),(50,2)]
+            convertR (1,97) (C 52 (50,48)) `shouldBe` [(1,49),(52,48)]
+            convertR (50,2) (C 52 (50,48)) `shouldBe` [(52,2)]
+            convertR (79,14) (C 52 (50,48)) `shouldBe` [(81,14)]
+            convertR (74,14) (C 45 (77,23)) `shouldBe` [(74,3),(45,11)]
+            convertR (45,11) (C 81 (45,19)) `shouldBe` [(81,11)]
+            convertR (45,11) (C 1 (0,69))`shouldBe` [(46,11)]
+
 
     describe "mapping" $ do 
         it "converts an int according to converters or itself if not in range" $ do
@@ -70,6 +87,29 @@ spec = do
             mapping 97 m `shouldBe` 99
             mapping 100 m `shouldBe` 100
 
+        it "converts ranges of ints within ranges of converters" $ do
+            let m = (maps sf) !! 0
+         -- 50 ← 98 2 / (1,99) → (1,97),(50,2)
+         -- 52 ← 50 48 / (1,97),(50,2) →  [(1,49),(52,48),(52,2)]
+            mappingR [(1,10)] m `shouldBe` [(1,10)]
+            mappingR [(1,10),(200,8)] m `shouldBe` [(1,10),(200,8)]
+            mappingR [(1,99),(100,199)] m `shouldBe` [(1,49),(52,46),(50,2),(50,2),(100,199)]
+            mappingR [(74,14)] (M [C 45 (77,23)])`shouldBe` [(74,3),(45,11)]
+            mappingR [(74,3),(45,11)] (M [C 81 (45,19)])`shouldBe` [(74,3),(81,11)]
+
+    describe "all MappingRs" $ do
+        it "converts ranges according to a group of mappings" $ do
+            allMappingsR [(seedRanges sf)!!0] (maps sf) `shouldBe` [(47,3),(46,11)]
+            let result = allMappingsRScan [(seedRanges sf)!!0] (maps sf)
+            zip result (maps sf) `shouldBe`
+                [([(79,14)],M [C {dest = 50, range = (98,2)},C {dest = 52, range = (50,48)}])
+                ,([(81,14)],M [C {dest = 0, range = (15,37)},C {dest = 37, range = (52,2)},C {dest = 39, range = (0,15)}])
+                ,([(81,14)],M [C {dest = 49, range = (53,8)},C {dest = 0, range = (11,42)},C {dest = 42, range = (0,7)},C {dest = 57, range = (7,4)}])
+                ,([(81,14)],M [C {dest = 88, range = (18,7)},C {dest = 18, range = (25,70)}])
+                ,([(74,14)],M [C {dest = 45, range = (77,23)},C {dest = 81, range = (45,19)},C {dest = 68, range = (64,13)}])
+                ,([(46,3),(45,11)],M [C {dest = 0, range = (69,1)},C {dest = 1, range = (0,69)}])
+                ,([(47,3),(46,11)],M [C {dest = 60, range = (56,37)},C {dest = 56, range = (93,4)}])]
+
     describe "lowest location" $ do
         it "find the lowest location after all mappings" $ do
             lowestLocation sf `shouldBe` 35 
@@ -78,3 +118,14 @@ spec = do
             puzzle <- lines <$> readFile "data/puzzle.txt"
             let al = readAlmanach puzzle
             lowestLocation al `shouldBe` 178159714
+
+    describe "lowest location range" $ do
+        it "find the lowest location after all mappings on all ranges" $ do
+            lowestLocationR sf `shouldBe` 46
+
+        it "solve the puzzle part two" $ do
+            puzzle <- lines <$> readFile "data/puzzle.txt"
+            let al = readAlmanach puzzle
+            lowestLocationR al `shouldBe` 0
+
+    
