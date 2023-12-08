c66656726cb3549f03f15eb1f76dbe8634878eac
ToF_ <cthibauttof@gmail.com>
2023-12-08 13:31:45 +0100

    toujours pas

--- day05/src/SeedFertilizer.hs
+++ day05/src/SeedFertilizer.hs
@@ -4,7 +4,11 @@ module SeedFertilizer
 import Data.Maybe
 import Data.List
 
-type Range = (Int,Int)
+type Length = Int
+type Interval = (Int, Int)
+
+data Range = R { start :: Int, length :: Length }
+    deriving (Eq, Show)
 
 data Almanach = Almanach { seeds      :: [Int],
                            maps       :: [Map] }
@@ -29,7 +33,7 @@ readAlmanach ss = Almanach { seeds      = map read (tail (words (head ss))),
         readMaps (M converters) (s:ss) =
             readMaps (M converters') ss
                 where
-                    converters' = converters <> [C { dest = dst, range = (srce, len) }]
+                    converters' = converters <> [C { dest = dst, range = R srce len }]
                     dst  = ints!!0
                     srce = ints!!1
                     len  = ints!!2
@@ -39,10 +43,10 @@ seedRanges :: Almanach -> [Range]
 seedRanges al = seedRanges' (seeds al)
     where
         seedRanges' [] = []
-        seedRanges' (s:l:ss) = (s,l) : seedRanges' ss
+        seedRanges' (s:l:ss) = R s l : seedRanges' ss
 
 convert :: Int -> Converter -> Maybe Int
-convert n (C dst (start,len)) =
+convert n (C dst (R start len)) =
     let offset = n - start
         within = offset >= 0 && offset < len
         result = dst + offset
@@ -61,27 +65,33 @@ lowestLocation sf = minimum $ foldl allMappings (seeds sf) [0..6]
     where
         allMappings seedList i = map (\seed -> mapping seed ((maps sf)!!i)) seedList
 
-isValidRange :: (Int, Int) -> Bool
-isValidRange (a,z) = a < z
+isValidInterval :: Interval -> Bool
+isValidInterval  = (Nothing /=) . rangeFromInterval
 
-validRange :: (Int,Int) -> Maybe Range
-validRange (a,z) = if isValidRange (a,z)
-                      then Just (a, z - a + 1)
-                      else Nothing
+intervalFromRange :: Range -> Interval
+intervalFromRange (R a l) = (a, a + l -1)
 
-offset :: Int -> (Int,Int) -> Range
-offset off (a,z) = if isValidRange (a,z) then (a + off, z + off) else (0,0)
+rangeFromInterval :: Interval -> Maybe Range
+rangeFromInterval (a,z) | a <= z = Just $ R a (z - a + 1)
+                        | otherwise = Nothing
+
+offset :: Int -> Interval -> Interval
+offset off (a,z) = if isValidInterval (a,z) then (a + off, z + off) else invalidInterval
+
+invalidInterval:: (Int, Int)
+invalidInterval = (1,-1)
 
 convertR :: Range -> Converter -> [Range]
-convertR (start,len) (C dst (srce, clen)) =
+convertR r (C dst cr) =
     let
-        (a0,z0) = (start, start + len - 1)
-        (a1,z1) = (srce, srce + clen - 1)
+        (a0,z0) = intervalFromRange r
+        (a1,z1) = intervalFromRange cr
         (a2,z2) = (max a0 a1, min z0 z1)
-        (a3,z3) = if isValidRange (a2,z2) then (a0,a1-1) else (0,0)
-        (a4,z4) = if isValidRange (a2,z2) then (z1+1,z0) else (0,0)
-    in case catMaybes $ map validRange [(a3,z3), offset (dst-srce) (a2,z2), (a4,z4)] of
-         [] -> [(start,len)]
+        (a3,z3) = if isValidInterval (a2,z2) then (a0,a1-1) else invalidInterval
+        (a4,z4) = if isValidInterval (a2,z2) then (z1+1,z0) else invalidInterval
+        R srce  _ = cr
+    in case catMaybes $ map rangeFromInterval [(a3,z3), offset (dst-srce) (a2,z2), (a4,z4)] of
+         [] -> [r]
          rs -> rs
 
 mappingR :: [Range] -> Map -> [Range]
@@ -90,12 +100,13 @@ mappingR rs (M (c:cs)) = foldr convertAllRanges rs (c:cs)
     where
     convertAllRanges c rs = concatMap (\r -> convertR r c) rs
 
-allMappingsR :: [Range] -> [Map] -> [Range]
-allMappingsR rs ms = foldl (\acc m -> mappingR acc m) rs ms
-
-allMappingsRScan :: [Range] -> [Map] -> [[Range]]
-allMappingsRScan rs ms = scanl (\acc m -> mappingR acc m) rs ms
+allMappingsR :: [Range] -> [Map] -> [[Range]]
+allMappingsR [] _ = []
+allMappingsR rs [] = [rs]
+allMappingsR rs (m:ms) = result : allMappingsR result ms
+    where
+        result = mappingR rs m
 
 lowestLocationR :: Almanach -> Int
-lowestLocationR sf = minimum $ map fst $ allMappingsR (seedRanges sf) (maps sf)
+lowestLocationR sf = minimum $ map start $ last $ allMappingsR (seedRanges sf) (maps sf)
 
