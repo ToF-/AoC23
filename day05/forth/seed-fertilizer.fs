afb327f05d0d311f9f92df1dc1850051b0391f17
Christophe Thibaut <cthibauttof@gmail.com>
2023-12-14 23:14:53 +0100

    hasrange addrange nextrange forth

--- day05/forth/seed-fertilizer.fs
+++ day05/forth/seed-fertilizer.fs
@@ -1,20 +1,67 @@
 2500 constant maxrange
 
-: range-set ( <name> -- )
-    create 0 ,
-    maxrange 0 do 0 0 2, loop ;
+: set ( n <name> -- )
+    create 0 , , ;
 
-: next-range ( range-set -- range|true )
-    dup @ 0= if
-        drop false
+: itemsize ( set -- n )
+    cell+ @ ;
+
+: itemcount ( set - n )
+    @ ;
+
+: set>items ( set - adr )
+    cell+ cell+ ;
+
+: rangeset ( <name> -- )
+    2 set ;
+
+: set>end ( set -- adr )
+    dup 2@ * cells
+    swap set>items + ;
+
+: (hasrange?) ( n,m,set -- f )
+    >r false -rot r>
+    dup set>items
+    swap set>end
+    swap do
+        i 2@ 
+        2over d= if
+            2drop drop true 0 0
+        then
+    2 cells +loop
+    2drop ;
+
+: hasrange? ( n,m,set -- f )
+    dup itemcount if
+        (hasrange?)
     else
-        cell+ 2@ true
+        drop 2drop false
     then ;
 
-: add-range ( range-set, range -- )
-    rot dup @                  \ range,range-set,count
-    2dup cells 2* +            \ range,range-set,count,dest
-    -rot 1+ swap !             \ range,dest
-    2! ;
-        
-           
+: (addrange) ( n,m,set -- )
+    dup itemcount 1+ over !
+    set>end 2 cells - 2! ;
+
+: addrange ( n,m,set -- )
+    dup 2over rot hasrange? 0= if
+        (addrange)
+    else
+        drop 2drop
+    then ;
+
+: (nextrange) ( set -- n,m)
+    dup set>items 2@ 2>r
+    dup itemcount 1- over !
+    dup set>items
+    swap set>end
+    swap ?do
+        i dup 2 cells + 2@ rot 2!
+    2 cells +loop
+    2r> ;
+
+: nextrange ( set -- n,m|f )
+    dup itemcount if
+        (nextrange) true
+    else
+        drop false
+    then ;
--- day05/forth/tests.fs
+++ day05/forth/tests.fs
@@ -2,14 +2,27 @@ require ffl/tst.fs
 require seed-fertilizer.fs
 
 t{
-    maxrange 2500 ?S
+    ." max range is 2500" cr
+    maxrange 2500 ?s
 
-    range-set foo
-    foo dbg next-range false ?S
+    ." a range set is initially empty" cr
+    rangeset foo
+    foo itemcount 0 ?s
+    
+    ." items in a range set are 2 cells long" cr
+    foo itemsize 2 ?s
+    
+    ." after adding a range to a range set the range set has it"
+    23 17 foo hasrange? ?false
+    23 17 foo addrange
+    23 17 foo hasrange? ?true
+    49 15 foo hasrange? ?false
+    49 15 foo addrange
+    49 15 foo hasrange? ?true
+    23 17 foo hasrange? ?true
 
-    foo 100 dump
-    foo 79 48 dbg add-range
-    foo dbg next-range ?true 79 48 ?D
-    foo 100 dump
+    ." nextrange pops a range from the range set"
+    foo nextrange ?true 23 17 ?d
+    foo nextrange ?true 49 15 ?d
 }t
 bye
